simoncozens/vharfbuzz

commit a552aaa41d00c9df6d4ef36167475bbc36ce598e

GlyphPosition is a namedtuple, make use of that

lib/vharfbuzz/__init__.py
@@ -260,11 +260,11 @@ class Vharfbuzz:
 
         defs = {}
         for info, pos in zip(buf.glyph_infos, buf.glyph_positions):
-            dx, dy = pos.position[0], pos.position[1]
+            dx, dy = pos.x_offset, pos.y_offset
             p = self._glyph_to_svg(info.codepoint, x_cursor + dx, y_cursor + dy, defs)
             paths.append(p)
-            x_cursor += pos.position[2]
-            y_cursor += pos.position[3]
+            x_cursor += pos.x_advance
+            y_cursor += pos.y_advance
 
         svg = [
             f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {x_cursor} {fullheight}" transform="matrix(1 0 0 -1 0 0)">',

commit e5729ccb9b37c8d49ec511ad94a9f8127324c8d0

Fix FakeItem.position
Broke in a552aaa41d00c9df6d4ef36167475bbc36ce598e

lib/vharfbuzz/__init__.py
@@ -222,9 +222,15 @@ class Vharfbuzz:
             info.cluster = int(groups[1])
             buf.glyph_infos.append(info)
             pos = FakeItem()
-            pos.position = [
-                int(x or 0) for x in (groups[3], groups[4], groups[6], 0)
-            ]  # Sorry, vertical scripts
+            pos.x_offset, pos.y_offset, pos.x_advance, pos.y_advance = pos.position = [
+                int(x or 0)
+                for x in (
+                    groups[3],
+                    groups[4],
+                    groups[6],
+                    0,  # Sorry, vertical scripts
+                )
+            ]
             buf.glyph_positions.append(pos)
         return buf
 